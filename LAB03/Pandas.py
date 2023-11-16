3a33825a81ef814a1f5f3d080e4825aa8e88e4ec
Mateusz Piechnik <mateuszpiechnik123@gmail.com>
2023-11-16 14:27:10 +0100

    pandas done

--- LAB03/Pandas.py
+++ LAB03/Pandas.py
@@ -1,7 +1,6 @@
 import pandas
-import imodels
-import numpy as np
 from datasets import load_dataset
+import matplotlib.pyplot as plt
 
 dataset = load_dataset("imodels/credit-card")
 df = pandas.DataFrame(dataset['train'])
@@ -20,13 +19,35 @@ df_without_duplicates.drop_duplicates(inplace=True)
 
 ##corelation
 
-#print("Correlation between age and bal limit is: ", df['limit_bal'].corr(df['age']))
+print("Correlation between age and bal limit is: ", df['limit_bal'].corr(df['age']))
 
 ##adding new column
-#df['sum_of_bill_amt'] = df['bill_amt1'] + df['bill_amt2'] + df['bill_amt3'] + df['bill_amt4'] + df['bill_amt5'] + df['bill_amt6']
+df['sum_of_bill_amt'] = df['bill_amt1'] + df['bill_amt2'] + df['bill_amt3'] + df['bill_amt4'] + df['bill_amt5'] + df['bill_amt6']
 #print(df['sum_of_bill_amt'])
 
 ##sort by age
 df_sort_by_age = df.sort_values(by='age', ascending=False)
-print(df_sort_by_age['age'].head(10))
 
+education_columns = df_sort_by_age[['education:1', 'education:2', 'education:3', 'education:4', 'education:5', 'education:6']].head(10)
+education_dictionary = {1: "graduate school", 2: "university", 3: "high school", 4: "others", 5: "unknown", 6: "unknown"}
+for i, column in enumerate(education_columns, 1):
+    if (df_sort_by_age[column] == 1.0).any():
+        df_sort_by_age.loc[df_sort_by_age[column] == 1.0, 'education'] = education_dictionary[i]
+
+print((df_sort_by_age[['limit_bal', 'age', 'education', 'sum_of_bill_amt']].head(10)).to_markdown(index=False))
+
+
+#histogram
+fig, axs = plt.subplots(3, 1, figsize=(10,15))
+axs[0].hist(df['limit_bal'], bins=120)
+axs[0].set_title('histogram limitu kredytu')
+
+axs[1].hist(df['age'], bins=60)
+axs[1].set_title('histogram wieku')
+
+axs[2].scatter( df['age'], df['limit_bal'],)
+axs[2].set_title('Zależność limitu kredytu od wieku')
+axs[2].set_xlabel('Wiek')
+axs[2].set_ylabel('Limit kredytu')
+
+plt.show()
